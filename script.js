b6e6874bd55312b47a4055e2baf43595836a23ca
alimukhtor <james1999007@gmail.com>
2021-10-28 16:26:19 +0200

    added some functionalities

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let loadUser = () => {
-  fetch('https://striveschool-api.herokuapp.com/books')
-  .then(response => response.json())
+window.onload = () => {
+  let getRow = document.querySelector(.row)
+  let getBtn1 = getRow.querySelectorAll(".btns:nth-child(1)")
+  for(let i = 0; i < getBtn1.length; i++){
+    const currentBtn = getBtn1[i]
+    currentBtn.innerText = "Hide";
+  }
 
-  .then(data => console.log(data));
 }
-loadUser()
